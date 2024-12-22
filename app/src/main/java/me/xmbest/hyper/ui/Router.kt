17f19e008ef85599a6a8ed7a7aab6712e9abdffc
Akimlc <1908459261@qq.com>
2024-12-22 16:17:46 +0800

    feat:添加过渡动画

--- app/src/main/java/me/xmbest/hyper/ui/Router.kt
+++ app/src/main/java/me/xmbest/hyper/ui/Router.kt
@@ -1,19 +1,52 @@
 package me.xmbest.hyper.ui
 
+import androidx.compose.animation.core.CubicBezierEasing
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import me.xmbest.hyper.cons.RouterCons
+import top.yukonga.miuix.kmp.utils.getWindowSize
 
 @Composable
 fun Router(modifier: Modifier) {
     val navController = rememberNavController()
+    val easing = FastOutSlowInEasing
     NavHost(
         navController = navController,
         startDestination = RouterCons.HOME,
-        modifier = modifier
+        modifier = modifier,
+        enterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { it },
+                animationSpec = tween(durationMillis = 500, easing = easing)
+            )
+        },
+        exitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { -it / 5 },
+                animationSpec = tween(durationMillis = 500, easing = easing)
+            )
+        },
+        popEnterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { -it / 5 },
+                animationSpec = tween(durationMillis = 500, easing = easing)
+            )
+        },
+        popExitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { it },
+                animationSpec = tween(durationMillis = 500, easing = easing)
+            )
+        }
     ) {
 
         RouterCons.getCompList(navController).forEach { page ->
